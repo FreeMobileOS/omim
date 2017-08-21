f8bb5480bf03ecddddfc51a075b70733d9f1577a
Satish Patel <tosatishpatel@gmail.com>
2017-08-21 12:24:08 +0530

    Add arm64-v8a support
    - add arm64-v8a support to relevant mk and gradle files
    - Fix build issue to cpp file
    - 64-bit support added to qmake configuration
    - changed android API-15 to 21 for 64-bit build support from NDK
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
 NDK_TOOLCHAIN_VERSION := clang
-APP_PLATFORM := android-15
+APP_PLATFORM := android-21
 APP_STL := c++_static
 
 # libc++-specific issues: -std=c++11" is turned on by default.
@@ -8,7 +8,7 @@ APP_STL := c++_static
 APP_CPPFLAGS += -Wno-deprecated-register
 
 ifeq (x$(NDK_ABI_TO_BUILD), x)
-  APP_ABI := armeabi-v7a x86
+  APP_ABI := armeabi-v7a x86 arm64-v8a
 else
   APP_ABI := $(NDK_ABI_TO_BUILD)
 endif
@@ -19,6 +19,11 @@ APP_CFLAGS += -I$(LOCAL_PATH)/../../3party/boost \
               -I$(LOCAL_PATH)/../../3party/glm
 
 APP_GNUSTL_FORCE_CPP_FEATURES := exceptions rtti
+
+ifeq ($(OMIM_ANDROID_64), 1)
+    LOCAL_CFLAGS += -DOMIM_ANDROID_64
+endif
+
 
 ifeq ($(NDK_DEBUG),1)
   APP_OPTIM := debug
